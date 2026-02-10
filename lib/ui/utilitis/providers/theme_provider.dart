6502a6f3967c35ee1297b51b79d17ef0f3581940
Islam Mohamed <islamm34@gmail.com>
2026-02-10 13:36:40 +0200

    create theme provider

---
+++ lib/ui/utilitis/providers/theme_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier{
+  ThemeMode themeMode = ThemeMode.light;
+
+  changeTheme(ThemeMode newThemeMode) {
+    themeMode = newThemeMode;
+    notifyListeners();
+  }
+}
